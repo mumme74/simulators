e60a24e6fec17947f2ec5b9be19322e318828a86
Fredrik Johansson <fredrik@mumme.se>
2022-02-12 23:17:07 +0100

    Bug fixes for Base and point

--- elements/base.mjs
+++ elements/base.mjs
@@ -4,26 +4,14 @@ import { Point } from "./point.mjs";
 
 export class BaseShape {
   offset = null;
+  _points = [];
 
   constructor({parentElement, rootElement, startPoint, className}) {
     this.offset = startPoint;
+    this._points.push(startPoint);
     this.node = rootElement;
     this.node.setAttribute("class", className);
     parentElement.appendChild(this.node);
-  }
-}
-
-export class Polygon extends BaseShape {
-  _points = [];
-
-  constructor({parentElement, points, className}) {
-    const rootElement = document.createElementNS('http://www.w3.org/2000/svg', "polygon");
-    rootElement.setAttribute("points", points.map(p=>`${p.x},${p.y}`).join(" "));
-    const svgPts = rootElement.points;
-    super({parentElement, rootElement, startPoint: svgPts[0], className});
-
-    for(const svgPt of svgPts)
-      this._points.push(new Point({svgPntRef: svgPt}));
   }
 
   get points() {
@@ -36,5 +24,20 @@ export class Polygon extends BaseShape {
             y = !isNaN(pt.y) ? pt.y : 0;
       this._points.setPoint({x, y});
     }
+  }
+}
+
+export class Polygon extends BaseShape {
+
+  constructor({parentElement, points, className}) {
+    const rootElement = document.createElementNS('http://www.w3.org/2000/svg', "polygon");
+    rootElement.setAttribute("points", points.map(p=>`${p.x},${p.y}`).join(" "));
+    const svgPts = rootElement.points;
+    const pt = new Point({svgPntRef: svgPts[0]});
+
+    super({parentElement, rootElement, startPoint: pt, className});
+
+    for(let i = 1; i < svgPts.length; ++i)
+        this._points.push(new Point({svgPntRef: svgPts[i]}));
   }
 }
--- elements/point.mjs
+++ elements/point.mjs
@@ -4,10 +4,10 @@ export class Point {
   _x = 0;
   _y = 0;
 
-  constructor({x=0, y=0, svgPntRef = null}) {
-    this._x = x;
-    this._y = y;
-    this.pntRef = svgPntRef;
+  constructor({x, y, svgPntRef = null}) {
+    this._x = !isNaN(x) ? x : svgPntRef ? svgPntRef.x : 0;
+    this._y = !isNaN(y) ? y : svgPntRef ? svgPntRef.y : 0;
+    this._pntRef = svgPntRef;
   }
 
   get point() {
@@ -26,23 +26,25 @@ export class Point {
   }
 
   set x(xVlu) {
-    this._x = x;
+    this._x = xVlu;
+    this._updated();
   }
   get x() {
     return this._x;
   }
 
   set y(yVlu) {
-    this._y = y;
+    this._y = yVlu;
+    this._updated();
   }
   get y() {
     return this._y;
   }
 
   _updated() {
-    if (this.pntRef) {
-      this._pntRef.x.baseValue.value = this._x;
-      this._pntRef.y.baseValue.value = this._y;
+    if (this._pntRef) {
+      this._pntRef.x = this._x;
+      this._pntRef.y = this._y;
     }
   }
 }
