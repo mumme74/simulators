95c5a744c5813ad001171305a881c85fac17a7f2
Fredrik Johansson <fredrik@mumme.se>
2022-02-13 13:38:51 +0100

    Changed API for base object, moved points into BaseShape

--- elements/base.mjs
+++ elements/base.mjs
@@ -6,12 +6,47 @@ export class BaseShape {
   offset = null;
   _points = [];
 
-  constructor({parentElement, rootElement, startPoint, className}) {
-    this.offset = startPoint;
-    this._points.push(startPoint);
+  constructor({parentElement, rootElement, points, className}) {
+    for (let i = 0; i < points.length; ++i) {
+      const pt = points[i];
+      if (pt instanceof Point)
+        this._points.push(pt);
+      else {
+        const svgPntRef = rootElement?.points[i];
+        this._points.push(new Point({x:pt.x, y:pt.y, svgPntRef}));
+      }
+    }
+    this.offset = this._points[0];
     this.node = rootElement;
     this.node.setAttribute("class", className);
     parentElement.appendChild(this.node);
+  }
+
+  move(newPoint) {
+    const newX = Array.isArray(newPoint) ? newPoint[0] : newPoint.x,
+          newY = Array.isArray(newPoint) ? newPoint[1] : newPoint.y;
+    const xDiff = !isNaN(newX) ? newX - this.offset.x : 0,
+          yDiff = !isNaN(newY) ? newY - this.offset.y : 0;
+    for(const pt of this._points)
+      pt.point = [pt.x + xDiff, pt.y + yDiff];
+  }
+
+  scale({factor=1, xFactor=null, yFactor=null}) {
+    xFactor = xFactor === null ? factor : xFactor;
+    yFactor = yFactor === null ? factor : yFactor;
+
+    // dont move our scaled object, only scale it
+    const anchorPt = this._points[0];
+    const xDiff = (anchorPt.x * xFactor) - anchorPt.x,
+          yDiff = (anchorPt.y * yFactor) - anchorPt.y;
+
+    for(let i = 1; i < this._points.length; ++i) {
+      const pt = this._points[i];
+      pt.point = [
+        pt.x * xFactor - xDiff,
+        pt.y * yFactor - yDiff
+      ];
+    }
   }
 
   get points() {
@@ -19,10 +54,13 @@ export class BaseShape {
   }
 
   set points(points) {
+    let newIter = 0;
     for (const pt of this.points) {
-      const x = !isNaN(pt.x) ? pt.x : 0,
-            y = !isNaN(pt.y) ? pt.y : 0;
-      this._points.setPoint({x, y});
+      if (points.length === newIter) break;
+      const newPt = points[newIter++];
+      const x = !isNaN(newPt.x) ? newPt.x : 0,
+            y = !isNaN(newPt.y) ? newPt.y : 0;
+      pt.point = {x, y};
     }
   }
 }
@@ -32,12 +70,7 @@ export class Polygon extends BaseShape {
   constructor({parentElement, points, className}) {
     const rootElement = document.createElementNS('http://www.w3.org/2000/svg', "polygon");
     rootElement.setAttribute("points", points.map(p=>`${p.x},${p.y}`).join(" "));
-    const svgPts = rootElement.points;
-    const pt = new Point({svgPntRef: svgPts[0]});
 
-    super({parentElement, rootElement, startPoint: pt, className});
-
-    for(let i = 1; i < svgPts.length; ++i)
-        this._points.push(new Point({svgPntRef: svgPts[i]}));
+    super({parentElement, rootElement, points: rootElement.points, className});
   }
 }
--- test/testBase.mjs
+++ test/testBase.mjs
@@ -10,13 +10,16 @@ const glbl = {
 };
 
 registerTestSuite("testBaseShape", ()=>{
-  const createShape = (className)=>{
+  const createShape = (points = [{x:0,y:0}], className)=>{
     const shp = document.createElementNS('http://www.w3.org/2000/svg', "polygon");
-    shp.setAttribute("points", "0,0")
+    shp.setAttribute("points", points.map(p=>`${p.x},${p.y}`).join(" "));
     glbl.point = new Point({svgPntRef: shp.points[0]});
+    const pts = [];
+    for(const p of shp.points) pts.push(p);
+    pts[0] = glbl.point;
     glbl.shape = new BaseShape({
       parentElement: glbl.parentElement, rootElement: shp,
-      startPoint: glbl.point,
+      points: pts,
       className
     });
   };
@@ -25,6 +28,7 @@ registerTestSuite("testBaseShape", ()=>{
     if (glbl.shape) {
       glbl.parentElement.removeChild(glbl.shape.node);
       glbl.shape = null;
+      glbl.point = null;
     }
   });
 
@@ -35,16 +39,216 @@ registerTestSuite("testBaseShape", ()=>{
       expect(glbl.shape.offset).toEqual(glbl.point);
     });
     it("Should construct a shape with className set", ()=>{
-      createShape("testClassName");
+      createShape(undefined, "testClassName");
       expect(glbl.shape.node.parentElement).toEqual(glbl.parentElement);
       expect(glbl.shape.offset).toEqual(glbl.point);
       expect(document.querySelector(".testClassName")).toEqual(glbl.shape.node);
     })
+  })
+
+  describe("Test BaseShape points", ()=>{
     it("Should get points", ()=>{
       createShape();
       const pnts = glbl.shape.points;
       expect(pnts.length).toBe(1);
       expect(pnts[0]).toBeObj({x:0,y:0});
+    });
+    it("Should move offset", ()=>{
+      createShape([{x:0,y:0}, {x:10,y:20}]);
+      glbl.shape.offset.point = {x:1,y:3};
+      expect(glbl.shape.node.points[0]).toBeObj({x:1,y:3});
+      expect(glbl.shape.node.points[1]).toBeObj({x:10,y:20});
+      expect(glbl.shape.offset).toBeObj({x:1,y:3});
+
+      glbl.shape.points = [{x:10,y:20},{x:1,y:3}];
+      expect(glbl.shape.node.points[0]).toBeObj({x:10,y:20});
+      expect(glbl.shape.node.points[1]).toBeObj({x:1,y:3});
+      expect(glbl.shape.offset).toBeObj({x:10,y:20});
+    });
+    it("Should move point", ()=>{
+      createShape();
+      glbl.shape.points = [{x:1,y:3}];
+      expect(glbl.shape.node.points[0]).toBeObj({x:1,y:3});
+
+      glbl.shape.points = [{x:10,y:20},{x:1,y:3}];
+      expect(glbl.shape.node.points[0]).toBeObj({x:10,y:20});
+    });
+
+    it("Should move shape", ()=>{
+      createShape([{x:0,y:0}, {x:10,y:20}, {x:30, y:40}]);
+      glbl.shape.move({x:1,y:3});
+      expect(glbl.shape.node.points[0]).toBeObj({x:1,y:3});
+      expect(glbl.shape.node.points[1]).toBeObj({x:11,y:23});
+      expect(glbl.shape.node.points[2]).toBeObj({x:31,y:43});
+      expect(glbl.shape.offset).toBeObj({x:1,y:3});
+    });
+    it("Should move shape with array", ()=>{
+      createShape([{x:0,y:0}, {x:10,y:20}, {x:30, y:40}]);
+      glbl.shape.move([1,3]);
+      expect(glbl.shape.node.points[0]).toBeObj({x:1,y:3});
+      expect(glbl.shape.node.points[1]).toBeObj({x:11,y:23});
+      expect(glbl.shape.node.points[2]).toBeObj({x:31,y:43});
+      expect(glbl.shape.offset).toBeObj({x:1,y:3});
+    });
+    it("Should move shape only x", ()=>{
+      createShape([{x:0,y:0}, {x:10,y:20}, {x:30, y:40}]);
+      glbl.shape.move({x:1});
+      expect(glbl.shape.node.points[0]).toBeObj({x:1,y:0});
+      expect(glbl.shape.node.points[1]).toBeObj({x:11,y:20});
+      expect(glbl.shape.node.points[2]).toBeObj({x:31,y:40});
+      expect(glbl.shape.offset).toBeObj({x:1,y:0});
+    });
+    it("Should move shape only y", ()=>{
+      createShape([{x:0,y:0}, {x:10,y:20}, {x:30, y:40}]);
+      glbl.shape.move({y:1});
+      expect(glbl.shape.node.points[0]).toBeObj({x:0,y:1});
+      expect(glbl.shape.node.points[1]).toBeObj({x:10,y:21});
+      expect(glbl.shape.node.points[2]).toBeObj({x:30,y:41});
+      expect(glbl.shape.offset).toBeObj({x:0,y:1});
+    });
+  });
+
+  describe("Test baseShape scale", ()=>{
+    it("Should scale x only", ()=>{
+      createShape([{x:0,y:0}, {x:10,y:20}, {x:30, y:40}]);
+      glbl.shape.scale({xFactor:2});
+      expect(glbl.shape.node.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.node.points[1]).toBeObj({x:20,y:20});
+      expect(glbl.shape.node.points[2]).toBeObj({x:60,y:40});
+      expect(glbl.shape.offset).toBeObj({x:0,y:0});
     })
+    it("Should scale y only", ()=>{
+      createShape([{x:0,y:0}, {x:10,y:20}, {x:30, y:40}]);
+      glbl.shape.scale({yFactor:2});
+      expect(glbl.shape.node.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.node.points[1]).toBeObj({x:10,y:40});
+      expect(glbl.shape.node.points[2]).toBeObj({x:30,y:80});
+      expect(glbl.shape.offset).toBeObj({x:0,y:0});
+    })
+    it("Should scale x and y", ()=>{
+      createShape([{x:0,y:0}, {x:10,y:20}, {x:30, y:40}]);
+      glbl.shape.scale({factor:2});
+      expect(glbl.shape.node.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.node.points[1]).toBeObj({x:20,y:40});
+      expect(glbl.shape.node.points[2]).toBeObj({x:60,y:80});
+      expect(glbl.shape.offset).toBeObj({x:0,y:0});
+    })
+    it("Should scale x and Y xFactor and yFactor", ()=>{
+      createShape([{x:0,y:0}, {x:10,y:20}, {x:30, y:40}]);
+      glbl.shape.scale({xFactor:2, yFactor: 4});
+      expect(glbl.shape.node.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.node.points[1]).toBeObj({x:20,y:80});
+      expect(glbl.shape.node.points[2]).toBeObj({x:60,y:160});
+      expect(glbl.shape.offset).toBeObj({x:0,y:0});
+    })
+    it("Should scale x with offset point", ()=>{
+      createShape([{x:10,y:20}, {x:20,y:40}, {x:40, y:60}]);
+      glbl.shape.scale({xFactor:2});
+      expect(glbl.shape.node.points[0]).toBeObj({x:10,y:20});
+      expect(glbl.shape.node.points[1]).toBeObj({x:30,y:40});
+      expect(glbl.shape.node.points[2]).toBeObj({x:70,y:60});
+      expect(glbl.shape.offset).toBeObj({x:10,y:20});
+    })
+    it("Should scale y with offset point", ()=>{
+      createShape([{x:10,y:20}, {x:20,y:40}, {x:40, y:60}]);
+      glbl.shape.scale({yFactor:2});
+      expect(glbl.shape.node.points[0]).toBeObj({x:10,y:20});
+      expect(glbl.shape.node.points[1]).toBeObj({x:20,y:60});
+      expect(glbl.shape.node.points[2]).toBeObj({x:40,y:100});
+      expect(glbl.shape.offset).toBeObj({x:10,y:20});
+    })
+    it("Should scale x and y with offset point", ()=>{
+      createShape([{x:10,y:20}, {x:20,y:40}, {x:40, y:60}]);
+      glbl.shape.scale({factor:2});
+      expect(glbl.shape.node.points[0]).toBeObj({x:10,y:20});
+      expect(glbl.shape.node.points[1]).toBeObj({x:30,y:60});
+      expect(glbl.shape.node.points[2]).toBeObj({x:70,y:100});
+      expect(glbl.shape.offset).toBeObj({x:10,y:20});
+    });
+    it("Should scale with offset point x and y scale differ", ()=>{
+      createShape([{x:10,y:20}, {x:20,y:40}, {x:40, y:60}]);
+      glbl.shape.scale({xFactor:2, yFactor:4});
+      expect(glbl.shape.node.points[0]).toBeObj({x:10,y:20});
+      expect(glbl.shape.node.points[1]).toBeObj({x:30,y:100});
+      expect(glbl.shape.node.points[2]).toBeObj({x:70,y:180});
+      expect(glbl.shape.offset).toBeObj({x:10,y:20});
+    });
   })
+});
+
+registerTestSuite("testPolygon", ()=>{
+  const createShape = (points, className)=>{
+    glbl.shape = new Polygon({
+      parentElement: glbl.parentElement,
+      points, className
+    });
+  };
+
+  afterEach(()=>{
+    if (glbl.shape) {
+      glbl.parentElement.removeChild(glbl.shape.node);
+      glbl.shape = null;
+      glbl.point = null;
+    }
+  });
+
+  describe("Test Constructing Polygon", ()=>{
+    it("Should construct a Line", ()=>{
+      createShape([{x:0,y:0}, {x:100,y:200}]);
+      expect(glbl.shape.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.points[1]).toBeObj({x:100,y:200});
+      expect(glbl.shape.points.length).toBe(2);
+    });
+    it("Should construct a Line with className", ()=>{
+      createShape([{x:0,y:0}, {x:100,y:200}], "testClassName");
+      expect(glbl.shape.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.points[1]).toBeObj({x:100,y:200});
+      expect(glbl.shape.points.length).toBe(2);
+      expect(document.querySelector(".testClassName")).toBe(glbl.shape.node);
+    });
+    it("Should construct a Triangle", ()=>{
+      createShape([{x:2,y:4},{x:100,y:200},{x:50, y:100}]);
+      expect(glbl.shape.points[0]).toBeObj({x:2,y:4});
+      expect(glbl.shape.points[1]).toBeObj({x:100,y:200});
+      expect(glbl.shape.points[2]).toBeObj({x:50,y:100});
+      expect(glbl.shape.points.length).toBe(3);
+    });
+    it("Should construct a 4corner", ()=>{
+      createShape([{x:2,y:4},{x:100,y:200},{x:50, y:100},{x:10,y:20}]);
+      expect(glbl.shape.points[0]).toBeObj({x:2,y:4});
+      expect(glbl.shape.points[1]).toBeObj({x:100,y:200});
+      expect(glbl.shape.points[2]).toBeObj({x:50,y:100});
+      expect(glbl.shape.points[3]).toBeObj({x:10,y:20});
+      expect(glbl.shape.points.length).toBe(4);
+    })
+  });
+
+  describe("Test moving Polygon", ()=>{
+    it("Should move first pt in a Line", ()=>{
+      createShape([{x:0,y:0}, {x:100,y:200}]);
+      expect(glbl.shape.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.points[1]).toBeObj({x:100,y:200});
+      expect(glbl.shape.points.length).toBe(2);
+      glbl.shape.points[0].point = [40,50];
+      expect(glbl.shape.points[0]).toBeObj({x:40,y:50});
+      expect(glbl.shape.points[1]).toBeObj({x:100,y:200});
+      expect(glbl.shape.node.points[0]).toBeObj({x:40,y:50});
+      expect(glbl.shape.node.points[1]).toBeObj({x:100,y:200});
+      expect(glbl.shape.offset).toBeObj({x:40,y:50});
+      expect(glbl.shape.points.length).toBe(2);
+    });
+    it("Should move second pt in a Line", ()=>{
+      createShape([{x:0,y:0}, {x:100,y:200}]);
+      expect(glbl.shape.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.points[1]).toBeObj({x:100,y:200});
+      expect(glbl.shape.points.length).toBe(2);
+      glbl.shape.points[1].point = [40,50];
+      expect(glbl.shape.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.points[1]).toBeObj({x:40,y:50});
+      expect(glbl.shape.node.points[0]).toBeObj({x:0,y:0});
+      expect(glbl.shape.node.points[1]).toBeObj({x:40,y:50});
+      expect(glbl.shape.offset).toBeObj({x:0,y:0});
+      expect(glbl.shape.points.length).toBe(2);
+    });
+  });
 });
