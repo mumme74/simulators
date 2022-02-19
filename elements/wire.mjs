4a3718330948fff09e45d17b66e14f4f4d554772
Fredrik Johansson <fredrik@mumme.se>
2022-02-19 07:42:02 +0100

    Wire feature complete

--- elements/wire.mjs
+++ elements/wire.mjs
@@ -10,11 +10,14 @@ export class Wire extends Polyline {
       new Point({x: endPnt.x, y: endPnt.y})
     ];
     if (startPnt instanceof Point)
-      points[0].followPoint(startPnt);
+      points[0].followPoint = startPnt;
     if (endPnt instanceof Point)
-      points[1].followPoint(endPnt);
+      points[1].followPoint = endPnt;
 
     super({parentElement, points, className});
+
+    this.recalculate.bound = this.recalculate.bind(this);
+    points.forEach(p=>p._onChangeCallbacks.push(this.recalculate.bound))
 
     this.betweenPnts = betweenPnts;
   }
@@ -51,56 +54,28 @@ export class Wire extends Polyline {
   }
 
   set betweenPnts(points) {
-    let i = 0;
-    for (; i < Math.min(points.length, this._points.length); ++i)
-      this._points[i].point = points[i];
-    // delete old pnts that is are not needed
-    while(Math.max(i,2) < this._points.length) { // we rely on removeitem shortens array
-      const pt = this._points[i];
-      pt.detachEverything();
-      this.removePoint(pt);
-    }
-    if (i < points.length) {
-      // insert new ones
-      const startPnt = this.startPnt, endPnt = this.endPnt;
-      for(;i < points.length; ++i) {
-        const pt = points[i];
-        if ((pt.x === startPnt && pt.y === startPnt.y) ||
-            (pt.x === endPnt.x && pt.y === endPnt.y))
-            continue; // don't insert a between point over start or endPoints
-        this.insertPoint(
-          this._createBetweenPnt(pt.x, pt.y, true), i+1); // +1 to not mess with startPoint
-      }
-    }
-    this.recalculate();
+    points = points.map(p=>{
+      if (!(p instanceof Point))
+        p = new Point({x:p.x, y:p.y});
+      p.pinned=true
+      p.addChangeCallback(this.recalculate.bound);
+      return p;
+    });
+
+    this.points = this._autoMiddlePoints(
+      [this.startPnt, ...points, this.endPnt]);
   }
 
   // finds shortest distance and
   recalculate() {
     // find all pinned points
-    const pinnedPts = [this.startPnt];
-    for(let i = 0; i < this._points.length; ++i) {
-      const pt = this._points[i];
-      if (pt.pinned) pinnedPts.push(pt); // i+1 for startPnt not accounted for
-    }
-    pinnedPts.push(this.endPnt);
+    const pinnedPts = [
+      this.startPnt,
+      ...this._points.filter(p=>p.pinned===true),
+      this.endPnt
+    ];
 
-    // track from first to last pinned points
-    const newPts = [];
-    for(let i = 0; i < pinnedPts.length-1; ++i) { // -1 to account for pt1 idx
-      const pt0 = pinnedPts[i], pt1 = pinnedPts[i+1];
-      const xDiff = pt1.x - pt0.x,
-            yDiff = pt1.y - pt0.y;
-      newPts.push(pt0);
-      if (xDiff !== 0 && yDiff !== 0) {
-        // insert a middle point between the 2
-        const x = (xDiff > yDiff) ? pt1.x : pt0.x;
-        const y = (xDiff > yDiff) ? pt0.y : pt1.y;
-        newPts.push(this._createBetweenPnt(x, y, false));
-      }
-      newPts.push(pt1);
-    }
-    this.points = newPts;
+    this.points = this._autoMiddlePoints(pinnedPts);
   }
 
   /**
@@ -110,28 +85,58 @@ export class Wire extends Polyline {
   connections() {
     const visited = [], endPts = [];
     // recursivly look up ends connected to all sub wires
-    const endLookUp = (wire, whichEnd) => {
-      for (const pnt of wire[whichEnd].connectedPoints()) {
+    const endLookUp = (wire, point) => {
+      for (const pnt of point.connectedPoints()) {
         if (pnt.owner instanceof Wire) {
           if (visited.indexOf(pnt) === -1) {
             visited.push(pnt);
-            endLookUp(pnt.owner, whichEnd);
+            if (pnt.owner !== wire) {
+              for (const p of pnt.owner._points)
+                endLookUp(pnt.owner, p);
+            }
           }
         } else if (endPts.indexOf(pnt) === -1)
           endPts.push(pnt);
       }
     }
-    endLookUp(this, 'startPnt');
-    endLookUp(this, 'endPnt');
+    for (const p of this._points)
+      endLookUp(this, p);
 
     return endPts;
   }
 
-  _createBetweenPnt(x, y, pinned) {
-    const svgPt = this._svgElem.createSVGPoint();
-    svgPt.x = x; svgPt.y = y;
-    const newPt = new Point({svgPntRef:svgPt, owner:this});
-    newPt.pinned = pinned;
-    return newPt;
+  _autoMiddlePoints(pinnedPts) {
+    // track from first to last pinned points
+    const newPts = [];
+    let prevPnt = this.endPnt;
+
+    for(let i = 0; i < pinnedPts.length-1; ++i) { // -1 to account for pt1 idx
+      const pt0 = pinnedPts[i], pt1 = pinnedPts[i+1];
+      const xDiff = pt1.x - pt0.x,
+            yDiff = pt1.y - pt0.y;
+      newPts.push(pt0);
+      if (xDiff !== 0 && yDiff !== 0) {
+        // insert a middle point between the 2
+        let x, y;                   // p=prev, 0=pt0, n= new, 1=pt1
+                                    //    p
+                                    //    |
+        if (prevPnt.x === pt0.x) {  //    0
+          x = pt0.x;                //    |
+          y = pt1.y;                //    n-1
+        } else
+          if (prevPnt.y === pt0.y) {  // p-0-n
+          x = pt1.x;                  //     |
+          y = pt0.y;                  //     1
+        } else {
+          x = (xDiff > yDiff) ? pt1.x : pt0.x;
+          y = (xDiff > yDiff) ? pt0.y : pt1.y;
+        }
+        prevPnt = new Point({x, y,owner:this});
+        newPts.push(prevPnt);
+      } else
+        prevPnt = pt0;
+    }
+
+    return [...newPts, pinnedPts[pinnedPts.length-1]];
   }
 }
--- test/test_wire.mjs
+++ test/test_wire.mjs
@@ -4,26 +4,28 @@ import { Point } from "../elements/point.mjs";
 import { Wire } from "../elements/wire.mjs";
 
 const glbl = {
-  shape: null,
+  shapes: [],
   parentElement: document.querySelector("svg"),
   point: null,
 };
 
 registerTestSuite("testWire", ()=>{
   afterEach(()=>{
-    if (glbl.shape) {
-      glbl.parentElement.removeChild(glbl.shape.node);
-      glbl.shape = null;
+    if (glbl.shapes.length) {
+      for(const shp of glbl.shapes)
+        glbl.parentElement.removeChild(shp.node);
+      glbl.shapes = [];
       glbl.point = null;
     }
   });
 
   const createWire = ({startPnt, endPnt, betweenPnts, className})=>{
-    return glbl.shape = new Wire({
+    glbl.shapes.push(new Wire({
       parentElement: glbl.parentElement,
       startPnt, endPnt, betweenPnts,
       className
-    });
+    }));
+    return glbl.shapes[glbl.shapes.length-1];
   };
 
   describe("Test Wire construction", ()=>{
@@ -54,11 +56,318 @@ registerTestSuite("testWire", ()=>{
       expect(wire.startPnt).toNotBe(pt1);
       expect(wire.endPnt).toNotBe(pt2);
     });
-    it("Should construct with 2 attached points", ()=>{});
-    it("Should construct with betweenPoints", ()=>{});
+    it("Should construct horizontal with 2 attached points", ()=>{
+      const pt0 = new Point({x:1,y:2}),
+            pt1 = new Point({x:100, y:2});
+      const wire = createWire({startPnt:pt0,endPnt:pt1});
+      expect(wire.points.length).toBe(2);
+      expect(wire.points[0]).toBeObj({x:1,y:2});
+      expect(wire.startPnt.followPoint).toBe(pt0);
+      expect(wire.points[1]).toBeObj({x:100,y:2});
+      expect(wire.endPnt.followPoint).toBe(pt1);
+    });
+    it("Should construct vertical with 2 attached points", ()=>{
+      const pt0 = new Point({x:1,y:2}),
+            pt1 = new Point({x:1, y:200});
+      const wire = createWire({startPnt:pt0,endPnt:pt1});
+      expect(wire.points.length).toBe(2);
+      expect(wire.points[0]).toBeObj({x:1,y:2});
+      expect(wire.startPnt.followPoint).toBe(pt0);
+      expect(wire.points[1]).toBeObj({x:1,y:200});
+      expect(wire.endPnt.followPoint).toBe(pt1);
+    });
+    it("Should construct horizontal reversed", ()=>{
+      const pt0 = new Point({x:100,y:2}),
+            pt1 = new Point({x:1, y:2});
+      const wire = createWire({startPnt:pt0,endPnt:pt1});
+      expect(wire.points.length).toBe(2);
+      expect(wire.points[0]).toBeObj({x:100,y:2});
+      expect(wire.startPnt.followPoint).toBe(pt0);
+      expect(wire.points[1]).toBeObj({x:1,y:2});
+      expect(wire.endPnt.followPoint).toBe(pt1);
+    });
+    it("Should construct horizontal with 2 attached points", ()=>{
+      const pt0 = new Point({x:1,y:200}),
+            pt1 = new Point({x:1, y:2});
+      const wire = createWire({startPnt:pt0,endPnt:pt1});
+      expect(wire.points.length).toBe(2);
+      expect(wire.points[0]).toBeObj({x:1,y:200});
+      expect(wire.startPnt.followPoint).toBe(pt0);
+      expect(wire.points[1]).toBeObj({x:1,y:2});
+      expect(wire.endPnt.followPoint).toBe(pt1);
+    });
+    it("Should construct with a auto betweenPoint x", ()=>{
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:30,y:30}});
+      expect(wire.points.length).toBe(3); // middle point automatically inserted
+      expect(wire.startPnt).toBeObj({x:10,y:20});
+      expect(wire.points[1]).toBeObj({x:30,y:20});
+      expect(wire.endPnt).toBeObj({x:30,y:30});
+    });
+    it("Should construct with a auto betweenPoint y", ()=>{
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:20,y:40}});
+      expect(wire.points.length).toBe(3); // middle point automatically inserted
+      expect(wire.startPnt).toBeObj({x:10,y:20});
+      expect(wire.points[1]).toBeObj({x:10,y:40});
+      expect(wire.endPnt).toBeObj({x:20,y:40});
+    });
+    it("Should construct with a betweenPoints", ()=>{
+      const pt0 = new Point({x:1,y:2}),
+            pt1 = new Point({x:100, y:20}),
+            pt2 = new Point({x:50,y:10});
+      const wire = createWire({startPnt:pt0,endPnt:pt1, betweenPnts:[pt2]});
+      expect(wire.points.length).toBe(5);
+      expect(wire.points[0]).toBeObj({x:1,y:2});
+      expect(wire.startPnt.followPoint).toBe(pt0);
+      expect(wire.points[1]).toBeObj({x:50,y:2});
+      expect(wire.points[2]).toBeObj({x:50,y:10});
+      expect(wire.points[3]).toBeObj({x:50,y:20});
+      expect(wire.points[4]).toBeObj({x:100,y:20});
+      expect(wire.endPnt.followPoint).toBe(pt1);
+    });
   });
 
   describe("Test endpoints manipulation", ()=>{
-
+    it("Should auto edit betweenPoint x from startPoint", ()=>{
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:30,y:30}});
+      wire.startPnt.y -= 5;
+      expect(wire.points.length).toBe(3); // middle point automatically inserted
+      expect(wire.startPnt).toBeObj({x:10,y:15});
+      expect(wire.points[1]).toBeObj({x:30,y:15});
+      expect(wire.endPnt).toBeObj({x:30,y:30});
+    });
+    it("Should auto edit betweenPoint y from startPoint", ()=>{
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:20,y:40}});
+      wire.startPnt.x -= 5;
+      expect(wire.points.length).toBe(3); // middle point automatically inserted
+      expect(wire.startPnt).toBeObj({x:5,y:20});
+      expect(wire.points[1]).toBeObj({x:5,y:40});
+      expect(wire.endPnt).toBeObj({x:20,y:40});
+    });
+    it("Should auto edit betweenPoint x from endPoint", ()=>{
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:19,y:30}});
+      wire.endPnt.y += 5;
+      expect(wire.points.length).toBe(3); // middle point automatically inserted
+      expect(wire.startPnt).toBeObj({x:10,y:20});
+      expect(wire.points[1]).toBeObj({x:10,y:35});
+      expect(wire.endPnt).toBeObj({x:19,y:35});
+    });
+    it("Should auto edit betweenPoint y from endPoint", ()=>{
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:22,y:30}});
+      wire.endPnt.x += 5;
+      expect(wire.points.length).toBe(3); // middle point automatically inserted
+      expect(wire.startPnt).toBeObj({x:10,y:20});
+      expect(wire.points[1]).toBeObj({x:27,y:20});
+      expect(wire.endPnt).toBeObj({x:27,y:30});
+    });
+    it("Should auto edit betweenPoint x from startPoint pinned betweenPnt", ()=>{
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:30,y:30},
+                               betweenPnts:[{x:30,y:20}]});
+      wire.startPnt.y -= 5;
+      expect(wire.points.length).toBe(4); // middle point automatically inserted
+      expect(wire.startPnt).toBeObj({x:10,y:15});
+      expect(wire.points[1]).toBeObj({x:30,y:15});
+      expect(wire.points[2]).toBeObj({x:30,y:20});
+      expect(wire.endPnt).toBeObj({x:30,y:30});
+    });
   });
+
+  describe("Test betweePoints manipulation", ()=>{
+    it("Should add a betweenPnt", ()=>{
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:30,y:20}});
+      wire.betweenPnts = [{x:20,y:0}];
+      expect(wire.points.length).toBe(5);
+      expect(wire.startPnt).toBeObj({x:10,y:20});
+      expect(wire.points[1]).toBeObj({x:20,y:20});
+      expect(wire.points[2]).toBeObj({x:20,y:0});
+      expect(wire.points[2].pinned).toEqual(true);
+      expect(wire.points[3]).toBeObj({x:20,y:20});
+      expect(wire.endPnt).toBeObj({x:30,y:20});
+    });
+    it("Should remove betweenPnt", ()=>{
+      const wire = createWire({
+        startPnt:{x:10,y:0}, endPnt:{x:30,y:20},
+        betweenPnts: [{x:20,y:10}]
+      });
+      wire.betweenPnts = [];
+      expect(wire.points.length).toBe(3); // middle point automatically inserted
+      expect(wire.startPnt).toBeObj({x:10,y:0});
+      expect(wire.points[1]).toBeObj({x:10,y:20});
+      expect(!!wire.points[1].pinned).toEqual(false);
+      expect(wire.endPnt).toBeObj({x:30,y:20});
+    });
+    it("Should move betweenPnt", ()=>{
+      const middlePnt = new Point({x:20,y:0});
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:30,y:20},
+        betweenPnts: [middlePnt]
+      });
+      expect(wire.points.length).toBe(5);
+      middlePnt.x += 5;
+      expect(wire.points.length).toBe(5);
+      expect(wire.startPnt).toBeObj({x:10,y:20});
+      expect(wire.points[1]).toBeObj({x:25,y:20});
+      expect(wire.points[2]).toBeObj({x:25,y:0});
+      expect(wire.points[2].pinned).toEqual(true);
+      expect(wire.points[3]).toBeObj({x:25,y:20});
+      expect(wire.endPnt).toBeObj({x:30,y:20});
+    });
+    it("Should add 3 betweenPnts", ()=>{
+      const wire = createWire({startPnt:{x:10,y:20}, endPnt:{x:30,y:20}});
+      wire.betweenPnts = [{x:20,y:20},{x:20,y:0},{x:20,y:20}];
+      expect(wire.points.length).toBe(5);
+      expect(wire.startPnt).toBeObj({x:10,y:20});
+      expect(wire.points[1]).toBeObj({x:20,y:20});
+      expect(wire.points[1].pinned).toEqual(true);
+      expect(wire.points[2]).toBeObj({x:20,y:0});
+      expect(wire.points[2].pinned).toEqual(true);
+      expect(wire.points[3]).toBeObj({x:20,y:20});
+      expect(wire.points[3].pinned).toEqual(true);
+      expect(wire.endPnt).toBeObj({x:30,y:20});
+    });
+  });
+
+  describe("Test connections", ()=>{
+    it("Should get endPnts of this wire and connected points", ()=>{
+      const pt0 = new Point({x:1,y:2}), pt1 = new Point({x:3,y:4});
+      const wire0 = createWire({startPnt:pt0, endPnt:pt1});
+      const conns = wire0.connections();
+      expect(conns.length).toBe(2);
+      expect(conns[0]).toBe(pt0);
+      expect(conns[1]).toBe(pt1);
+    });
+    it("Should have not connection", ()=>{
+      // wire has no connections
+      const wire = createWire({startPnt:{x:1,y:2}, endPnt:{x:3,y:4}});
+      const conns = wire.connections();
+      expect(conns.length).toBe(0);
+    });
+    it("Should have 1 connection", ()=>{
+      const pt0 = new Point({x:1,y:2});
+      const wire = createWire({startPnt:pt0, endPnt:{x:3,y:4}});
+      const conns = wire.connections();
+      expect(conns.length).toBe(1);
+      expect(conns[0]).toBe(pt0);
+    });
+    it("Should connect 2 wires in series", ()=>{
+      const pt0 = new Point({x:1,y:2}),
+            pt1 = new Point({x:5,y:6});
+      const wire0 = createWire({startPnt:pt0, endPnt:{x:3,y:4}}),
+            wire1 = createWire({startPnt:wire0.endPnt, endPnt:pt1});
+      const conns0 = wire0.connections(),
+            conns1 = wire1.connections();
+      expect(conns0.length).toBe(2);
+      expect(conns1.length).toBe(2);
+      expect(conns0[0]).toBe(pt0);
+      expect(conns0[1]).toBe(pt1);
+      expect(conns1[0]).toBe(pt0);
+      expect(conns1[1]).toBe(pt1);
+    });
+    it("Should connect 2 wires in parallell", ()=>{
+      const pt0 = new Point({x:1,y:2}),
+            pt1 = new Point({x:5,y:6});
+      const wire0 = createWire({startPnt:pt0, endPnt:pt1}),
+            wire1 = createWire({startPnt:wire0.endPnt, endPnt:wire0.startPnt});
+      const conns0 = wire0.connections(),
+            conns1 = wire1.connections();
+      expect(conns0.length).toBe(2);
+      expect(conns1.length).toBe(2);
+      expect(conns0[0]).toBe(pt0);
+      expect(conns0[1]).toBe(pt1);
+      expect(conns1[0]).toBe(pt1);
+      expect(conns1[1]).toBe(pt0);
+    });
+    it("Should connect 3 wires in series", ()=>{
+      const pt0 = new Point({x:1,y:2}),
+            pt1 = new Point({x:5,y:6});
+      const wire0 = createWire({startPnt:pt0}),
+            wire1 = createWire({startPnt:wire0.endPnt}),
+            wire2 = createWire({startPnt:wire1.endPnt, endPnt: pt1});
+      const conns0 = wire0.connections(),
+            conns1 = wire1.connections(),
+            conns2 = wire2.connections();
+      expect(conns0.length).toBe(2);
+      expect(conns1.length).toBe(2);
+      expect(conns2.length).toBe(2);
+      expect(conns0[0]).toBe(pt0);
+      expect(conns0[1]).toBe(pt1);
+      expect(conns1[0]).toBe(pt0);
+      expect(conns1[1]).toBe(pt1);
+      expect(conns2[0]).toBe(pt0);
+      expect(conns2[1]).toBe(pt1);
+    });
+    it("Should connect 3 wires in Y pattern", ()=>{
+      const pt0 = new Point({x:1,y:2, owner:0}),
+            pt1 = new Point({x:5,y:6, owner:1}),
+            pt2 = new Point({x:7,y:8, owner:2});
+      const wire0 = createWire({startPnt:pt0}),
+            wire1 = createWire({startPnt:wire0.endPnt, endPnt:pt1}),
+            wire2 = createWire({startPnt:wire0.endPnt, endPnt:pt2});
+      const conns0 = wire0.connections(),
+            conns1 = wire1.connections(),
+            conns2 = wire2.connections();
+      expect(conns0.length).toBe(3);
+      expect(conns1.length).toBe(3);
+      expect(conns2.length).toBe(3);
+      expect(conns0).toContain(pt0);
+      expect(conns0).toContain(pt1);
+      expect(conns0).toContain(pt2);
+      expect(conns1).toContain([pt0,pt1,pt2]);
+      expect(conns2).toContain([pt0,pt1,pt2]);
+    });
+    it("Should connect 3 wires in N pattern", ()=>{
+      const pt0 = new Point({x:1,y:2, owner:0}),
+            pt1 = new Point({x:5,y:6, owner:1}),
+            pt2 = new Point({x:7,y:8, owner:2});
+      const wire0 = createWire({startPnt:pt0}),
+            wire1 = createWire({startPnt:wire0.startPnt, endPnt:pt1}),
+            wire2 = createWire({startPnt:wire1.endPnt, endPnt:pt2});
+      const conns0 = wire0.connections(),
+            conns1 = wire1.connections(),
+            conns2 = wire2.connections();
+      expect(conns0).toContain([pt0,pt1,pt2]);
+      expect(conns1).toContain([pt0,pt1,pt2]);
+      expect(conns2).toContain([pt0,pt1,pt2]);
+    });
+    it("Should connect 3 wires in H pattern", ()=>{
+      const pt0 = new Point({x:1,y:2, owner:0}),
+            pt1 = new Point({x:2,y:12, owner:1}),
+            pt2 = new Point({x:10,y:2, owner:2}),
+            pt3 = new Point({x:11,y:12, owner:3});
+      const wire0 = createWire({startPnt:pt0, endPnt:pt1}),
+            wire1 = createWire({startPnt:pt2, endPnt:pt3}),
+            wire2 = createWire({startPnt:wire0.points[1],
+                                endPnt:wire1.points[1]});
+      const conns0 = wire0.connections(),
+            conns1 = wire1.connections(),
+            conns2 = wire2.connections();
+      expect(conns0.length).toBe(4);
+      expect(conns1.length).toBe(4);
+      expect(conns2.length).toBe(4);
+      expect(conns0).toContain([pt0,pt1,pt2,pt3]);
+      expect(conns1).toContain([pt0,pt1,pt2,pt3]);
+      expect(conns2).toContain([pt0,pt1,pt2,pt3]);
+    });
+    it("Should connect 4 wires in M pattern", ()=>{
+      const pt0 = new Point({x:1,y:2, owner:0}),
+            pt1 = new Point({x:1,y:12, owner:1}),
+            pt2 = new Point({x:5,y:5, owner:2}),
+            pt3 = new Point({x:10,y:2, owner:3}),
+            pt4 = new Point({x:10,y:12, owner:4});
+      const wire0 = createWire({startPnt:pt0, endPnt:pt1}),
+            wire1 = createWire({startPnt:pt0, endPnt:pt2}),
+            wire2 = createWire({startPnt:pt2, endPnt:pt4}),
+            wire3 = createWire({startPnt:pt3, endPnt:pt4});
+      const conns0 = wire0.connections(),
+            conns1 = wire1.connections(),
+            conns2 = wire2.connections(),
+            conns3 = wire3.connections();
+      expect(conns0.length).toBe(5);
+      expect(conns1.length).toBe(5);
+      expect(conns2.length).toBe(5);
+      expect(conns3.length).toBe(5);
+      expect(conns0).toContain([pt0,pt1,pt2,pt3,pt4]);
+      expect(conns1).toContain([pt0,pt1,pt2,pt3,pt4]);
+      expect(conns2).toContain([pt0,pt1,pt2,pt3,pt4]);
+      expect(conns3).toContain([pt0,pt1,pt2,pt3,pt4]);
+    });
+  })
 });
