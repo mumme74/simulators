977ed15060e6d08ffcee60624a67fb1f5ff8dbae
Fredrik Johansson <fredrik@mumme.se>
2022-02-20 13:37:35 +0100

    Added a menu for all simulations

---
+++ menu.mjs
@@ -0,0 +1,273 @@
+"use strict";
+
+// add all possible tags here
+const tags = {
+  sim:'Simulator',
+  math:'Matte',
+  trigenometry: "Trigenometri",
+  electrical:"Ellära",
+  mechanical: "Mekanisk",
+  pneumatic:"Pneumatik",
+  hydraulic:"Hydraulik",
+  ac_electrical:'Växelströmslära',
+  motors:"Elmotor",
+};
+
+// tags can be in many categories
+const tagSubCategories = {
+  math: {
+    tags:[tags.trigenometry]
+  },
+  electrical: {
+    tags: [tags.motors, tags.ac_electrical],
+    ac_electrical: {
+      tags:[tags.motors]
+    }
+  },
+}
+
+// add a new page here
+const allPages = [
+  {
+    name:"Sin-Cos simulator", path:"sin-cos/sin-cos.html",
+    tags:[tags.math, tags.trigenometry, tags.sim]
+  },
+  {
+    name:"Synkron-motor", path:"bldc/motor_synkron.html",
+    tags:[tags.ac_electrical, tags.electrical, tags.motors]
+  }
+];
+
+// ------------------ menu logic from here on -------------------
+
+// sanitize key values
+tags._ = (name)=>{
+  // find tag key from tag string name
+  const entries = Object.entries(tags);
+  const itm = entries.find(e=>{if (e[1]===name) return e[0]});
+  if (itm) return itm[0];
+}
+
+tags.__ = (arr)=>{
+  // replace tag str names with keys instead
+  return arr.map(t=>tags._(t));
+}
+
+allPages.forEach(p=>p.tags=tags.__(p.tags));
+(function sani(obj) {
+  for(let o in obj) {
+    if (!obj.hasOwnProperty(o)) continue;
+    if (o === 'tags') obj[o] = tags.__(obj[o]);
+    else if (obj[o] !== undefined && typeof obj[o] === 'object')
+      sani(obj[o]);
+  }
+})(tagSubCategories);
+
+class MenuPopup {
+  _state = "startPage";
+  constructor(toggleNode) {
+    this.toggleNode = toggleNode;
+    this.root = document.createElement("div");
+    document.body.appendChild(this.root);
+    this.root.className = "menu-popup-root";
+    toggleNode.addEventListener("click", this.toggleShow.bind(this));
+
+    const header = document.createElement("header");
+    this.root.appendChild(header);
+
+    this.search = document.createElement("input");
+    this.search.type = "text";
+    this.search.placeholder = "Sök efter sida"
+    this.search.addEventListener("input", this.onSearch.bind(this));
+    header.appendChild(this.search);
+
+    const closer = document.createElement("div");
+    closer.appendChild(document.createTextNode("✖"));
+    closer.className = "closer-cross";
+    closer.addEventListener("click", this.hide.bind(this));
+    header.appendChild(closer);
+
+
+
+    this.displaySection = document.createElement("section");
+    this.displaySection.className = "menu-display-list";
+    this.root.appendChild(this.displaySection);
+    this.startMenu();
+  }
+
+  hide() {
+    const rect = this.root.getBoundingClientRect()
+    this.root.style.left = `-${rect.width}px`;
+  }
+
+  show() {
+    const rect = this.toggleNode.getBoundingClientRect();
+    this.root.style.left = `${rect.left}px`;
+    this.root.style.top = `${rect.top + rect.height}px`;
+    this.search.focus();
+  }
+
+  isShown(){
+    return this.root.getBoundingClientRect().left > 0;
+  }
+
+  toggleShow() {
+    this.isShown() ? this.hide() : this.show();
+  }
+
+  startMenu() {
+    this._state = "startPage";
+
+    const createLink = (page, li) => {
+      const a = document.createElement("a");
+      a.href = `/${page.path}`;
+      a.appendChild(document.createTextNode(page.name));
+      li.appendChild(a);
+      return a;
+    };
+
+    const createLi = (ul, title) =>{
+      const li = document.createElement("li");
+      ul.appendChild(li);
+      if (title) li.appendChild(document.createTextNode(title));
+      li.addEventListener("click", (evt)=>{
+        evt.stopPropagation();
+        const ul = li.querySelector("*>ul");
+        if (ul) {
+          const method = ul.classList.contains("folded") ? "remove" : "add";
+          ul.classList[method]("folded");
+          if (method === "remove") {
+            for(const n of evt.target.querySelectorAll("*>li.hidden"))
+              n.classList.remove("hidden");
+          }
+        }
+      });
+      return li;
+    }
+
+    const createUl = (parent) =>{
+      const ul = document.createElement("ul");
+      if (!parent.classList.contains("menu-category-header"))
+        ul.classList.add("folded");
+      parent.appendChild(ul);
+      return ul;
+    }
+
+    const createPageLinksForTag = (tag, ul) =>{
+      // lookup page
+      const pages = allPages.filter(p=>p.tags.indexOf(tag)>-1);
+      for (const p of pages) {
+        const li = createLi(ul);
+        const a = createLink(p, li);
+      }
+    }
+
+    this.displaySection.innerHTML = ""; // clear
+    const categories = createUl(this.displaySection);
+    categories.className = "menu-category-header";
+    categories.appendChild(document.createTextNode("Kategorier"));
+    const cb = (method)=>{ return (evt)=>{
+        evt.stopPropagation();
+        evt.target.classList[method]("currentHover");
+    }}
+    categories.addEventListener("mouseover", cb("add"));
+    categories.addEventListener("mouseout", cb("remove"));
+
+    const buildCatTree = (obj, ul) => {
+      const arr = !Array.isArray(obj) ? Object.keys(obj) : obj;
+      for(const o of arr) {
+        if (o === 'tags' && obj[o].length) {
+          for(const t of obj[o]) {
+            const li = createLi(ul, tags[t]);
+            const ul2 = createUl(li);
+            createPageLinksForTag(t, ul2);
+          }
+        } else if (obj[o] !== undefined && typeof obj[o] === 'object') {
+          const title = tags[o];
+          const li = createLi(ul, title);
+          const ul2 = createUl(li);
+          buildCatTree(obj[o], ul2);
+        }
+      }
+    }
+    buildCatTree(tagSubCategories, categories);
+
+    const aboutBtn = document.createElement("button");
+    aboutBtn.textContent = "Om"
+    aboutBtn.addEventListener("click", this.onAbout.bind(this));
+    this.displaySection.appendChild(aboutBtn);
+  }
+
+  onAbout() {
+    this._state = "aboutPage";
+    this.displaySection.innerHTML = `
+      <h3>Simulatorer & annat</h3>
+      <p>Detta är en samling simulatorer och andra saker
+      som är avsedda att användas i undervisning.</p>
+      <p>De tillhandahålls
+      som <strong>free/open source</strong> och skall ses som <i>free as in beer</i>,
+      utan några garantier.</p>
+      <p>Du får dock gärna hjälpa till med bugfixar, koda ny exempel etc på:
+        <a href="https://github.com/mumme74/simulators">Simulators på github</a>
+      </p>
+      <section class='about-licence'>
+      <h4>Copyright ${(new Date()).getFullYear()} Fredrik Johansson</h4>
+      <p>
+      Permission is hereby granted, free of charge, to any person obtaining a
+      copy of this software and associated documentation files (the "Software"),
+      to deal in the Software without restriction, including without limitation
+      the rights to use, copy, modify, merge, publish, distribute, sublicense,
+      and/or sell copies of the Software, and to permit persons to whom the Software
+      is furnished to do so, subject to the following conditions:
+      </p>
+      <p>
+      The above copyright notice and this permission notice shall be
+      included in all copies or substantial portions of the Software.
+      </p>
+      <p>
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+      </p>
+      </section>
+    `;
+    const btn = document.createElement("button");
+    btn.textContent = "Tillbaka";
+    btn.addEventListener("click", this.startMenu.bind(this));
+    this.displaySection.appendChild(btn);
+  }
+
+  onSearch(evt) {
+    if (this._state !== "startPage")
+      this.startMenu();
+
+    const needle = this.search.value;
+    if (!needle.length)
+      return this.startMenu();
+
+    const nodes = this.displaySection.querySelectorAll(
+      ".menu-category-header a");
+    for (const node of nodes) {
+      let n = node, found = false;
+      while(n !== this.displaySection && n) {
+        if (!found) {
+          const haystack = n.textContent.toLocaleLowerCase();
+          found = haystack.indexOf(needle.toLocaleLowerCase()) > -1;
+        }
+
+        if (n.tagName === 'UL' && !n.classList.contains('menu-category-header'))
+          n.classList[found ? "remove" : "add"]("folded");
+        else if (n.tagName === 'LI')
+          n.classList[found ? "remove" : "add"]("hidden");
+
+        // go up tree to remove/search for needle
+        n = n.parentElement;
+      }
+    }
+  }
+}
+
+const hideShowMenu = document.createElement("span");
+hideShowMenu.className = "toggle-menu-btn";
+hideShowMenu.appendChild(document.createTextNode('☰'));
+const header = document.querySelector("body > div:first-child > header");
+header.insertBefore(hideShowMenu, header.firstElementChild);
+const menu = new MenuPopup(hideShowMenu);
---
+++ sin-cos/sin-cos.mjs
@@ -0,0 +1,70 @@
+import { Triangle, Circle } from '../elements/trigenometry/index.mjs';
+import { Text, Line } from '../elements/base.mjs';
+import { Point } from '../elements/point.mjs';
+import { Wave } from '../elements/diagrams/index.mjs';
+import { toFraction } from '../helpers/index.mjs';
+
+const vlu = { deg:0, rad:0, sin:0, cos:1 };
+const maxDeg = 720;
+
+// create graphical elements
+const parentElement = document.querySelector("svg");
+const vLine = new Line({parentElement, point1:{x:450,y:50}, point2:{x:450,y:250}}),
+      hLine = new Line({parentElement, point1:{x:450,y:150}, point2:{x:850,y:150}});
+
+const circle = new Circle({
+  parentElement, centerPoint:{x:180,y:150},
+  radii:100, className:"circle"});
+const circleX = circle.offset.x,
+      circleY = circle.offset.y,
+      waveXScale = (circle.radii * 2) / 360;
+
+const sinPt = new Point({x:circleX + circle.radii, y:circleY}),
+      cosPt = new Point({x:sinPt.x, y:sinPt.y});
+const triangle = new Triangle({parentElement, className:"triangle", points:[
+                    circle.offset, cosPt, sinPt]}),
+      wave = new Wave({parentElement, offset:hLine.offset,
+        className:"wave", dataPoints:Array(720).fill(0), yScale:100, xScale: waveXScale});
+const degTxt = new Text({parentElement, text:"deg: 0°", followPoint:sinPt, offsetX:10, offsetY:-30}),
+      radianTxt = new Text({parentElement, text:"radian", followPoint:sinPt, offsetX:10, offsetY:-8}),
+      sinTxt = new Text({parentElement, text:"sin", followPoint:sinPt, offsetX:10, offsetY:15}),
+      cosTxt = new Text({parentElement, text:"cos", followPoint:cosPt, offsetX:-65, offsetY:15});
+const ruler = new Line({parentElement, point1:new Point({followPoint:sinPt}), className: "ruler",
+                        point2:{x:hLine.offset.x, y:hLine.offset.y}});
+
+const waveData = wave.dataPoints;
+
+sinPt.addChangeCallback(()=>{
+  cosPt.x = sinPt.x;
+  // move ruler
+  const x = wave.offset.x + vlu.deg * waveXScale;
+  ruler.point2.point = [x, sinPt.y];
+  // update wave
+  const radFactor = Math.PI / 180;
+  for(let i = 0; i < vlu.deg; ++i)
+    waveData[i] = Math.sin(i * radFactor);
+  waveData[vlu.deg] = vlu.sin;
+  waveData.fill(0, vlu.deg+1);
+  // update texts
+  const frac = toFraction({vlu:vlu.rad / Math.PI});
+  const intStr = frac.int > 0 ? ''+frac.int : '',
+        frStr = frac.num > 0 ? `${frac.num}/${frac.den}`: "";
+  degTxt.text = `deg: ${vlu.deg}°`;
+  radianTxt.text = `radian: ${intStr} ${frStr}&pi; (${vlu.rad.toPrecision(3)})`;
+  sinTxt.text = `sin: ${vlu.sin.toPrecision(3)}`;
+  cosTxt.text = `cos: ${vlu.cos.toPrecision(3)}`;
+});
+
+const slider = document.querySelector("input[type=range]");
+
+slider.addEventListener("input", function(evt) {
+  vlu.deg = +this.value;
+  vlu.rad = (+this.value) * (Math.PI / 180);
+  vlu.sin = Math.sin(vlu.rad);
+  vlu.cos = Math.cos(vlu.rad);
+  if (vlu.sin < 0.01 && vlu.sin > -0.01) vlu.sin = 0.0;
+  if (vlu.cos < 0.01 && vlu.cos > -0.01) vlu.cos = 0.0;
+
+  const radii = circle.radii;
+  sinPt.point = [circleX + vlu.cos * radii, circleY - vlu.sin * radii];
+});
